658c0cb4956bdeeef1eb06c868012e7435e32bcf
Emily-Xue Kang <xkang3@dons.usfca.edu>
2017-01-31 22:34:44 -0800

    basic functions implemented

--- src/cs652/repl/JavaREPL.java
+++ src/cs652/repl/JavaREPL.java
@@ -1,35 +1,26 @@
 package cs652.repl;
 
 import java.io.*;
-import java.nio.Buffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import javax.tools.*;
 import com.sun.source.util.JavacTask;
-import javafx.beans.binding.ObjectExpression;
 
 import java.io.IOException;
-import java.lang.reflect.Method;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 
 
 public class JavaREPL {
-	private static Path DEFAULT_PATH;
+	private static Path DEFAULT_DIR;
 	private static ClassLoader loader;
+	private static String PRINT_PREFIX = "print ";
 
 	public static void main(String[] args) throws IOException {
-		DEFAULT_PATH = Files.createTempDirectory("temp");
-		loader = new URLClassLoader(new URL[]{DEFAULT_PATH.toUri().toURL()});
+		DEFAULT_DIR = Files.createTempDirectory("temp");
+		loader = new URLClassLoader(new URL[]{DEFAULT_DIR.toUri().toURL()});
 		exec(new InputStreamReader(System.in));
 	}
 
@@ -37,94 +28,97 @@ public class JavaREPL {
 		BufferedReader stdin = new BufferedReader(r);
 		NestedReader reader = new NestedReader(stdin);
 		int classNumber = 0;
+		String statement = "";
+		String declaration = "";
+		String newcode;
+		String className;
+		String extendSuper;
 
-
-		while (true) {  //while not end of file, if isdeclaration, save java file decl; else save java file statments, then exec
+		while (true) {
+			//while not end of file, if isDeclaration, save java file decl; else save java file statements, then exec
 			System.out.print("> ");
 			String java = reader.getNestedString();
+			if (java.length() == 0) continue;
 
-            if(java.length() == 0) continue;
-
-            String className;
-            String extendSuper;
-
-            if(classNumber != 0){
-                extendSuper = "Interp_" + (classNumber-1);
-                className = "Interp_" + classNumber;
-            }
-            else{
-                className = "Interp_" + classNumber;
-                extendSuper = "";
-            }
-
-			String statement = "";
-            String declaration = "";
-			String newcode;
-
-            if (isDeclaration(java)){
-                declaration = java;
-                newcode = getCode(className, extendSuper,declaration,statement);
-            }
-			else{
-			    statement = java;
-			    newcode = getCode(className,extendSuper,declaration,statement);
+			if (classNumber != 0) {
+				extendSuper = "Interp_" + (classNumber - 1);
+				className = "Interp_" + classNumber;
+			} else {
+				className = "Interp_" + classNumber;
+				extendSuper = "";
 			}
 
-			writeFile(DEFAULT_PATH.toString(), className, newcode);
-            boolean success = compile(className);
-			if (success) exec(loader, className, "exec");
-            classNumber++;
+			statement = "";
+			declaration = "";
+			if (isDeclaration(java)) {
+				declaration = java;
+				newcode = getCode(className, extendSuper, declaration, statement);
+			} else {
+				statement = java;
+				newcode = getCode(className, extendSuper, declaration, statement);
+			}
 
-        }
+			File classFile = writeFile(className, newcode);
+			boolean success = compile(classFile);
+			if (success) exec(loader, className, "exec");
+			classNumber++;
+		}
 
 	}
 
-
-	public static boolean isDeclaration(String line) throws IOException{
+	/**
+	 * this method is used to compile any input lina as a declaration.
+	 *
+	 * @param line--need to transform to a javafile of declaration class and put it into a file.
+	 * @return whether it can be compiled. see the result of "diagnostics.getDiagnostics().size()".
+	 * @throws IOException
+	 */
+	public static boolean isDeclaration(String line) throws IOException {
+		String lineTocode = "import java.io.*;\n" + "import java.util.*;\n" + "public class Bogus {\n" + "public static " + line + "\n" + "public static void exec() {\n}\n}";
+		String tempFileName = "Bogus.java";
+		File file = writeFile(tempFileName, lineTocode);
 		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 		DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
 		StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null);
-		Iterable<? extends JavaFileObject> compilationUnits = fileManager
-				.getJavaFileObjectsFromStrings(Arrays.asList(line));
+		String classPath = DEFAULT_DIR.toAbsolutePath().toString();
+		String[] compileOptions = new String[]{"-d", classPath, "-cp", classPath};
+		Iterable<String> compilationOptions = Arrays.asList(compileOptions);
+		Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjects(file);
 		JavacTask task = (JavacTask)
-				compiler.getTask(null, fileManager, diagnostics,
-						null, null, compilationUnits);
+				compiler.getTask(null, fileManager, diagnostics, compilationOptions, null, compilationUnits);
 		task.parse();
+
 		return diagnostics.getDiagnostics().size() == 0;
-
-//		getTask(Writer out, JavaFileManager fileManager, DiagnosticListener<? super JavaFileObject> diagnosticListener, Iterable<String> options, Iterable<String> classes, Iterable<? extends JavaFileObject> compilationUnits)
-//		Creates a future for a compilation task with the given components and arguments.
-
 	}
 
-    /**
-     * Return a class code based on the input line, with classname, declaration or statement.
-     * @param className
-     * @param extendSuper
-     * @param declaration
-     * @param statement
-     * @return
-     */
-	public static String getCode(String className, String extendSuper, String declaration, String statement){
+	/**
+	 * Return a class code based on the input line, with classname, declaration or statement.
+	 *
+	 * @param className
+	 * @param extendSuper
+	 * @param declaration
+	 * @param statement
+	 * @return
+	 */
+	public static String getCode(String className, String extendSuper, String declaration, String statement) {
 
-	    String javacode = "";
+		String javacode = "";
 		String importing = "import java.io.*;\n" + "import java.util.*;\n";
 
 		String executable = "public static void exec() {\n" + statement + "\n}\n";
 		String classbody = "public static " + declaration + "\n" + executable;
 
-		if(!statement.equals("")){
-		    classbody = executable;
-        }
-        if(!declaration.equals("")){
-		    classbody = "public static " + declaration + "\n" + "public static void exec() {\n}\n";
-        }
-        if(className.equals("Interp_0")){
-            javacode = importing + "public class " + className + " {\n" + classbody + "\n}";
-        }
-        else{
-            javacode = importing + "public class " + className + " extends " + extendSuper + " {\n" + classbody + "\n}";
-        }
+		if (statement.length() != 0) {
+			classbody = executable;
+		}
+		if (declaration.length() != 0) {
+			classbody = "public static " + declaration + "\n" + "public static void exec() {\n}\n";
+		}
+		if (className.equals("Interp_0")) {
+			javacode = importing + "public class " + className + " {\n" + classbody + "\n}";
+		} else {
+			javacode = importing + "public class " + className + " extends " + extendSuper + " {\n" + classbody + "\n}";
+		}
 
 		return javacode;
 	}
@@ -135,96 +129,69 @@ public class JavaREPL {
 	 * and http://www.informit.com/articles/article.aspx?p=2027052&seqNum=2
 	 * Modified by XueKang
 	 */
-	public static Boolean compile(String className) throws IOException {
-		String filename = DEFAULT_PATH.toString() + "/" + className + ".java";
+	public static Boolean compile(File classFile) throws IOException {
 		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 		DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
 		StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null);
-		Iterable<? extends JavaFileObject> compilationUnits = fileManager
-				.getJavaFileObjectsFromStrings(Arrays.asList(filename)); //Construct an in-memory java source file from dynamic code
-		String[] compileOptions = new String[]{"-g", "-d", "-cp"} ;//need to be verified
+		Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjects(classFile);
+		//need to be verified,replace with actual directory and classpath
+		String classPath = DEFAULT_DIR.toAbsolutePath().toString();
+		String[] compileOptions = new String[]{"-d", classPath, "-cp", classPath};
 		Iterable<String> compilationOptions = Arrays.asList(compileOptions);
 		JavacTask task = (JavacTask)
-				compiler.getTask(null, fileManager, diagnostics,
-						compilationOptions, null, compilationUnits);
+				compiler.getTask(null, fileManager, diagnostics, compilationOptions, null, compilationUnits);
+
 		boolean success = task.call();
 		fileManager.close();
+		if (diagnostics.getDiagnostics().size() != 0) {
+			System.out.println(diagnostics.getDiagnostics());
+		}
 		return success;
 	}
-//	public static CompilerControl getCompilerControlObject(String fileName){
-//
-//	}
-	public static void exec(ClassLoader loader, String className, String methodName){
-        try{
-        	Class cl = loader.loadClass(className);
+
+	public static void exec(ClassLoader loader, String className, String methodName) {
+		try {
+			Class cl = loader.loadClass(className);
 			Method f1 = cl.getDeclaredMethod(methodName);
 
 			Object o = cl.newInstance();
-			f1.invoke(null,null);
-		}catch (Exception e){
-        	System.out.println(e.getStackTrace());
+			f1.invoke(null, null);
+		} catch (Exception e) {
+			System.out.println(e.getStackTrace());
 		}
 
 	}
 
 	/**
 	 * Reference sourse: https://www.mkyong.com/java/how-to-write-to-file-in-java-bufferedwriter-example/
-	 * @param dir
-	 * @param fileName
+	 *
+	 * @param className
 	 * @param content
 	 */
-	public static void writeFile(String dir, String fileName, String content){
+	public static File writeFile(String className, String content) {
 		//SimpleJavaFileObject file = new SimpleJavaFileObject();
 		BufferedWriter bw = null;
 		FileWriter fw = null;
-		String FILENAME = "\\" + fileName + ".java";
+		String fileName = className + ".java";
+		File parentDir = DEFAULT_DIR.toFile();
+		File file = new File(parentDir, fileName);
 		try {
-
-			fw = new FileWriter(FILENAME);
-			bw = new BufferedWriter(fw);
+			file.createNewFile();
+			bw = Files.newBufferedWriter(file.toPath());
 			bw.write(content);
-
-			//System.out.println("Done");
-
 		} catch (IOException e) {
-
 			e.printStackTrace();
-
 		} finally {
-
 			try {
-
 				if (bw != null)
 					bw.close();
 
 				if (fw != null)
 					fw.close();
-
 			} catch (IOException ex) {
-
 				ex.printStackTrace();
-
 			}
-
 		}
+		return file;
 	}
-
-    /**
-     * Copy from http://stackoverflow.com/questions/617414/how-to-create-a-temporary-directory-folder-in-java
-	 * Reference :http://docs.oracle.com/javase/7/docs/api/java/nio/file/Files.html#createTempDirectory%28java.nio.file.Path
-	 * ,%20java.lang.String,%20java.nio.file.attribute.FileAttribute...%29
-     * @return a
-     * @throws IOException
-     */
-    public static Path createTempDirectory()
-            throws IOException
-    {
-        final Path temp;
-
-        temp = Files.createTempFile("temp", Long.toString(System.nanoTime()));
-		
-        return (temp);
-    }
-
-
 }
